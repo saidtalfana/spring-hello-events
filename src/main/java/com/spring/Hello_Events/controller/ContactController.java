f3bac095ce29a1b00c73a8e2bec3cdae09c0b33d
oussama charafi <ou.charafi@gmail.com>
2024-07-26 11:41:58 +0100

    I added the last mdf

--- src/main/java/com/spring/Hello_Events/controller/ContactController.java
+++ src/main/java/com/spring/Hello_Events/controller/ContactController.java
@@ -5,6 +5,8 @@ import com.spring.Hello_Events.service.ContactService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/contact")
 public class ContactController {
@@ -15,5 +17,15 @@ public class ContactController {
     @PostMapping("/add/{id}")
     public Contact addContact(@RequestBody Contact contact, @PathVariable int id) {
         return contactService.addContact(contact, id);
+    }
+
+    @GetMapping("/get_all")
+    public List<Contact> getAllContacts() {
+        return contactService.getAllContacts();
+    }
+
+    @GetMapping("/get/{id}")
+    public List<Contact> getContact(@PathVariable int id) {
+        return contactService.getContactByIdUserId(id);
     }
 }
