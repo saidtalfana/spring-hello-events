7ae9deae101217b1127a45e71760607f4c2b9f7e
oussama charafi <ou.charafi@gmail.com>
2024-07-26 11:05:44 +0100

    I added the last mdf

--- src/main/java/com/spring/Hello_Events/repository/EventRepository.java
+++ src/main/java/com/spring/Hello_Events/repository/EventRepository.java
@@ -3,6 +3,16 @@ import com.spring.Hello_Events.model.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
+import java.util.List;
+
 @Repository
 public interface EventRepository extends JpaRepository<Event, Integer> {
+    List<Event> findByDate(Date date);
+    List<Event> findByLocation(String location);
+    List<Event> findByCategory(String category);
+    List<Event> findByDateAndLocation(Date date, String location);
+    List<Event> findByDateAndCategory(Date date, String category);
+    List<Event> findByLocationAndCategory(String location, String category);
+    List<Event> findByDateAndLocationAndCategory(Date date, String location, String category);
 }
